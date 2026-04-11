19ff4bcfcc0642f3a261a570daabc5413e577149
xavi <xpeiro92@gmail.com>
2026-04-11 22:37:33 +0200

    .

--- DSA/btree.go
+++ DSA/btree.go
@@ -1,83 +1,37 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+type Key = string
+type Value = string
+
+const T = 2
+
+// TODO: optimization -> keys array
+type Node struct {
+	keys []int `json:"keys,omitempty"`
+	leaf bool  `json:"leaf,omitempty"`
+}
+
+type BTree struct {
+	root Node
+}
+
+func (b BTree) NewBTree() *BTree {
+
+	root := Node{
+		keys: make([]int, 2*T-1),
+		leaf: true,
+	}
+	tree := BTree{
+		root: root,
+	}
+
+	return &tree
+}
+
+func (b BTree) Search() (Key, Value) {
+	return "", ""
+}
+func (b BTree) Insert() bool {}
 
 func main() {
-	db_path := "btreedb/index1/wena.db"
-
-	clear_btree(db_path)
-	for i := 0; i < 100; i++ {
-		add_element(db_path, strconv.Itoa(i), []byte("value"+strconv.Itoa(i)))
-
-	}
-
-	key := "33"
-	id, value := search_element(db_path, key)
-	println("key: %s, value: %s", id, value)
-}
-
-func search_element_in_memory(node_content string) string {
-
-}
-
-func search_element(db string, key string) (string, string) {
-	var _iterations uint8 = 0
-
-	file, _ := os.OpenFile(db, os.O_RDONLY, 0)
-
-	file.Seek(0, 0)
-	reader := bufio.NewReader(file)
-
-	var id, value string = "", ""
-	for _iterations < 10 {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Iterations number: %u", _iterations)
-		fmt.Println(line)
-		fmt.Println("-------------")
-		parts := strings.Split(line, ":")
-		id, value = parts[0], parts[1]
-
-		if id == key {
-			//if pointer keep_going
-			//else return
-			break
-		}
-
-		_iterations++
-	}
-
-	return id, value
-}
-
-func add_element(db string, key string, val []byte) {
-	file, _ := os.OpenFile(
-		db,
-		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
-		0644,
-	)
-
-	fmt.Fprintf(file, "%s:%s\n", key, val)
-
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(file)
-
-}
-
-func clear_btree(path string) {
-	os.RemoveAll(path)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module hash_table_go
+
+go 1.19
