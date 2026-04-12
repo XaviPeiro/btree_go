61209da0624dfb54b46226b7f6919fda79bc6186
Xavi PL <xpeiro92@gmail.com>
2026-04-12 20:07:40 +0200

    .

--- DSA/btree.go
+++ DSA/btree.go
@@ -3,12 +3,21 @@ package main
 type Key = string
 type Value = string
 
+// type I=
 const T = 2
+
+//type Entry struct {
+//	Key Key
+//	//Val Value
+//	LeftNode  Node
+//	RightNode Node
+//}
 
 // TODO: optimization -> keys array
 type Node struct {
-	keys []int `json:"keys,omitempty"`
-	leaf bool  `json:"leaf,omitempty"`
+	Keys     []Key //`json:"keys,omitempty"`
+	Children []*Node
+	Leaf     bool //`json:"leaf,omitempty"
 }
 
 type BTree struct {
@@ -16,10 +25,10 @@ type BTree struct {
 }
 
 func (b BTree) NewBTree() *BTree {
-
 	root := Node{
-		keys: make([]int, 2*T-1),
-		leaf: true,
+		Keys:     make([]Key, 2*T-1),
+		Leaf:     true,
+		Children: make([]*Node, 2*T-1),
 	}
 	tree := BTree{
 		root: root,
@@ -28,10 +37,29 @@ func (b BTree) NewBTree() *BTree {
 	return &tree
 }
 
-func (b BTree) Search() (Key, Value) {
-	return "", ""
+func (b BTree) Search(key Key, node *Node) (int, *Node) {
+	current_node := b.root
+	var i int = 0
+	// this On I guess it could be logn
+	for i < len(current_node.Keys) && key > current_node.Keys[i] {
+		i++
+	}
+
+	if key == current_node.Keys[i] {
+		return i, node
+	}
+	if current_node.Leaf == true {
+		return i, current_node
+	} else {
+		return b.Search(key, current_node.Children[i])
+	}
+
+	return ""
 }
-func (b BTree) Insert() bool {}
+func (b BTree) InsertNode(key Key) bool {
+	// if key does not exist
+	b.Search(key)
+}
 
 func main() {
 }
