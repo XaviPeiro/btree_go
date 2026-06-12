a9c7f0dbf7ba4409a3eb7dd85bcd0ef74d6930a5
xavi <xpeiro92@gmail.com>
2026-06-12 19:23:30 +0200

    .

--- DSA/btree.go
+++ DSA/btree.go
@@ -1,7 +1,41 @@
 package main
 
+/*
+BTree
+---
+
+# Invariants
+
+Most of them, if not all, are oriented to keep the tree balanced.
+
+## Children and keys
+All nodes have a mD (minimum degree, in other words, minimum number of children) named T.
+A MD (maximum degree) of 2T.
+
+By extension, mK (minimum keys) of T-1 and MK of 2T-1.
+
+Root node is the exception, it may has less than T.
+
+# Leafs
+A leaf is a node with no children.
+
+All leaves shall be at the very same level.
+
+
+# Insert
+Always at the bottom of the tree.
+When a node is full (2T-1 K) is divided by 2 and the remaining one is promoted to the parent (depth-1).
+This is done recursively.
+=> 
+- Ensures all leafs are at the very same level (so same depth).
+- *The filters adapt to the number of keys*; in the case of a monotonic one the keys promoted will the lastly entered,
+increasing the gap btw 'em (greater filter) and promoting them in concordance with the inserts.*
+I SHOULD ANALYZE THIS BETTER, the only clear thing I see is that it adapt to the 'hottest trend' by promoting them up. 
+For random keys as UUID it may be problematic.
+
+*/
 // type Key = string
-type Key = int // GEneralise later 
+type Key = int // Generalize later 
 type Value = string
 type Index = int
 type Found = bool
@@ -66,7 +100,8 @@ func (b *BTree) InsertInNodeIndex(node *Node, index Index, key Key) (bool, strin
     if len(node.keys) >= MaxKeys {
 
 		/*
-		
+		When full rebalance in 2 nodes: maxk=2T-1 mink=T-1 -> solo una posibilidad, 2 nodos	
+
 		*/
 		// We should rebalance, but isn't implemented yet
 		panic("Insert, node full should be split. Not implemented yet")
@@ -110,6 +145,10 @@ func (b *BTree) Insert(key Key) (bool, string) {
 	}
 
 	return false, "Key must be unique"
+}
+
+func (btree *BTree) SearchForInsert() {
+
 }
 
 func (b *BTree) Search(target_key Key, starting_node *Node) (*Node, Index, Found) {
